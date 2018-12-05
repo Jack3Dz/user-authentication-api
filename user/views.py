f9a63366fa6170d2bb1a566602035c3e1d46084b
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-05 20:52:04 -0200

    adding token to view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -15,6 +16,9 @@ class UserCreate(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
