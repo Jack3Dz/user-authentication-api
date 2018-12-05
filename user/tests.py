342c1a714adc0a240cca0ccd38ddae0c72aaa1d9
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-05 20:31:14 -0200

    tests for username validation and separating response_field validation

--- user/tests.py
+++ user/tests.py
@@ -38,10 +38,7 @@ class UsersTest(APITestCase):
             'password': 'foo'
         }
 
-        response = self.client.post(self.create_url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(len(response.data['password']), 1)
+        self.response_field(data, 'password')
 
     def test_create_user_with_no_password(self):
         data = {
@@ -50,9 +47,40 @@ class UsersTest(APITestCase):
             'password': 'foo'
         }
 
+        self.response_field(data, 'password')
+
+    def test_create_user_with_too_long_username(self):
+        data = {
+            'username': 'foo' * 30,
+            'email': 'foobarbaz@example.com',
+            'password': 'foobar'
+        }
+
+        self.response_field(data, 'username')
+
+    def test_create_user_with_no_username(self):
+        data = {
+            'username': '',
+            'email': 'foobarbaz@example.com',
+            'password': 'foobar'
+        }
+
+        self.response_field(data, 'username')
+
+    def test_create_user_with_preexisting_username(self):
+        data = {
+            'username': 'testuser',
+            'email': 'user@example.com',
+            'password': 'testuser'
+        }
+
+        self.response_field(data, 'username')
+
+    
+
+    def response_field(self, data, field):
         response = self.client.post(self.create_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(len(response.data['password']), 1)
+        self.assertEqual(len(response.data[field]), 1)
 
-    
