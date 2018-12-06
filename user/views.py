b68acc8f0d3f0fe1d48cc39cfddb1a7ed5aab92e
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-05 22:13:56 -0200

    adjust views to token

--- user/views.py
+++ user/views.py
@@ -1,9 +1,9 @@
-from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from user.serializers import UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 
 class UserCreate(APIView):
@@ -19,6 +19,6 @@ class UserCreate(APIView):
                 token = Token.objects.create(user=user)
                 json = serializer.data
                 json['token'] = token.key
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(json, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
