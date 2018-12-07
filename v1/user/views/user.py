2a2e7af628745db1051fa5d416a37a10aed0ee0d
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-07 07:25:05 -0200

    separating LoginView and UserView

---
+++ v1/user/views/user.py
@@ -0,0 +1,23 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from v1.user.serializers.serializers import UserSerializer
+from rest_framework.authtoken.models import Token
+
+
+class UserView(APIView):
+    """
+    Creates the user.
+    """
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
