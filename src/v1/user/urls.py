7fc1634ec8932efb55b6dff02692459402cba9a4
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-24 16:36:07 -0200

    adding profile to urls user app

--- src/v1/user/urls.py
+++ src/v1/user/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
+
+from src.v1.user.views.profile import ProfileView, ProfileDetail
 from .views.user import UserView
 from .views.login import LoginView
 from .views.logout import LogoutView
 
 
+
 urlpatterns = [
-    path('users/', UserView.as_view(), name='user-create'),
+
+    # Login / logout
     path('login/', LoginView.as_view(), name='user-login'),
     path('logout/', LogoutView.as_view(), name='user-logout'),
+
+    # Profiles
+    path('profiles', ProfileView.as_view(), name='profile-list'),
+    path('profiles/<int:id>/', ProfileDetail.as_view(), name='profile-specific'),
+
+    # Users
+    path('users/', UserView.as_view(), name='user-create'),
+
 ]
