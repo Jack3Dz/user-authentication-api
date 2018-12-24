73d0d33f2b1b31aa6e19e912d59f1b0c23c5e1ea
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-24 16:35:33 -0200

    changing the name of serializer to user

---
+++ src/v1/user/serializers/user.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
+from src.v1.user.models.profile import Profile
+from rest_framework.authtoken.models import Token
+from .profile import ProfileSerializer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = serializers.SerializerMethodField()
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        max_length=32,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(min_length=6, write_only=True)
+
+    @staticmethod
+    def get_profile(user):
+        """
+        Get or create profile
+        """
+
+        profile, created = Profile.objects.get_or_create(user=user)
+        return ProfileSerializer(profile, read_only=True).data
+
+    def create(self, validated_data):
+        user = User(email=validated_data['email'],
+                    username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'profile')
+
+
+class UserSerializerLogin(UserSerializer):
+    token = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_token(user):
+        """
+        Get or create token
+        """
+
+        token, created = Token.objects.get_or_create(user=user)
+        return token.key
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'profile', 'role', 'token')
